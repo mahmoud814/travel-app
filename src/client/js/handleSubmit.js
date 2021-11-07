3561ffeada12d6e55446b169c66e78851bba6736
mahmoud-RT <mahmoud.abdelrahman@rabbittec.com>
2021-11-07 09:45:29 +0200

    ui first handling

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,13 +1,19 @@
 // submit Button
 
 document.getElementById('submit').addEventListener('click' , makeRequests);
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
 function makeRequests (e){
+    event.preventDefault();
     window.scroll({
         top: document.body.scrollHeight,
         behavior: 'smooth'
     });
     var loadingDiv = document.getElementById("loading");
     loadingDiv.classList.add("active");
+    modal.style.display = "block";
+      
 //     e.preventDefault();
 //     const destinationInput = document.getElementById('zip').value;
 //     const remarksInput = document.getElementById('feelings').value;
@@ -29,3 +35,12 @@ function makeRequests (e){
 //   })
 
 }
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
