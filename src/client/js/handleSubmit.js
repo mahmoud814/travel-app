0c1581fa7bfc19b10cfd2405b14a2bf21ffd789f
mahmoud-RT <mahmoud.abdelrahman@rabbittec.com>
2021-11-07 13:30:15 +0200

    validation and test operation

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,46 +1,66 @@
+const { validate } = require("./validateForm");
+const { dayDiffrence } = require("./dayDiff.js");
+let modal = document.getElementById("myModal");
+
+
 // submit Button
 
-document.getElementById('submit').addEventListener('click' , makeRequests);
-var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
 
-function makeRequests (e){
-    event.preventDefault();
-    window.scroll({
+
+
+
+document.getElementById('submit').addEventListener('click' , (e)=>{
+   event.preventDefault();
+
+
+   // form data variables 
+
+   let departInput = document.querySelector('#depart-input').value;
+   let returnInput = document.querySelector('#return-input').value;
+   let destinationInput = document.querySelector('#destination-input').value;
+   let psngrInput = document.querySelector('#psngr-input').value;
+   let notesInput = document.querySelector('#notes-input').value;
+   var loadingDiv = document.getElementById("loading");
+
+
+
+   let tripDay = dayDiffrence(departInput, returnInput);
+
+
+   loadingDiv.classList.add("active");
+   console.log(departInput);
+   validate(departInput , returnInput , destinationInput);
+
+   window.scroll({
         top: document.body.scrollHeight,
         behavior: 'smooth'
     });
-    var loadingDiv = document.getElementById("loading");
-    loadingDiv.classList.add("active");
-    modal.style.display = "block";
+
+    // modal.style.display = "block";
       
-//     e.preventDefault();
-//     const destinationInput = document.getElementById('zip').value;
-//     const remarksInput = document.getElementById('feelings').value;
-//     console.log(destinationInput);
-//     fetch ('http://localhost:8000/all-apis', {
-//         method: 'POST',
-//         credentials: 'same-origin',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({destinationInput, remarksInput}) // Convert the input data of destination and remarks into a string for server
-//     })
+  //   fetch ('http://localhost:8000/all-apis', {
+  //       method: 'POST',
+  //       credentials: 'same-origin',
+  //       headers: {
+  //           'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({destinationInput, remarksInput}) // Convert the input data of destination and remarks into a string for server
+  //   })
 
-//     .then (res => {
-//         return res.json()
-//     })   
-//     .then (data => {
-//      console.log(data);
-//   })
+  //   .then (res => {
+  //       return res.json()
+  //   })   
+  //   .then (data => {
+  //    console.log(data);
+  // })
 
-}
+});
 
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+
+
+
